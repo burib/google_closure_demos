4c3354967760d381020d8e8569a30399b8517a4a
Balazs Buri <balazs_buri@epam.com>
2012-10-27 20:20:42 +0200

    moving cl to the right place, modifying tooltip and other demos, starting wysiwyg demo
    - moving cl to the right place,
    - modifying tooltip and other demos,
    - starting wysiwyg demo

---
+++ tooltip/js/tooltip.js
@@ -0,0 +1,15 @@
+goog.provide('tooltip'); //Providing your app namespace
+goog.require('goog.dom.dataset');
+goog.require('goog.ui.Tooltip');
+tooltip.init = function(){
+    var tooltipElements = goog.dom.getElementsByClass('tooltip'),
+        i = 0,
+        length = tooltipElements.length;
+
+    for (; i < length; i++) {
+        new goog.ui.Tooltip(tooltipElements[i]).setHtml(goog.dom.dataset.get(tooltipElements[i], 'tooltiptext'));
+    };
+}
+
+//make sure we can call this function from outside, even in advanced compilation mode.
+goog.exportSymbol('tooltip.init', tooltip.init);
